162c5cd3603e9a3e86920054102ac5590ddd9273
sutin <defsdfsdfsdfsf>
2019-05-05 08:46:31 -0700

    yes

--- Cog/Admin.py
+++ Cog/Admin.py
@@ -1,15 +1,19 @@
+import random
+
 import discord
 from discord.ext import commands
-import random
+
+
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
     async def ban(self, ctx, member : discord.Member, *, reason : str = None):
         try:
-            banned_user = member.mention
-            await ctx.user.send(f"Banned user **{banned_user}**, reason: {reason}")
-            member.ban(reason=reason)
+            await member.ban(reason=reason)
+            embed = discord.Embed(color=0x7027F0)
+            embed.add_field(name=f"Banned user: {member.display_name}", value=f"\n └Reason: {reason}")
+            await ctx.send(embed=embed)
         except discord.errors.Forbidden:
             NoPermList = [
                 "I do not have permission to use that command!",
@@ -18,7 +22,6 @@ class Admin(commands.Cog):
             ]
             await ctx.send(random.choice(NoPermList))
             return
-
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
@@ -29,5 +32,29 @@ class Admin(commands.Cog):
                 "Sorry! Please check your perms and try again",
             ]
             await ctx.send(random.choice(NoPermArray))
+            return
+
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason: str = None):
+        try:
+            await member.kick(reason=reason)
+            embed = discord.Embed(color=0x3A1B36)
+            embed.add_field(name=f"Kicked user: {member.display_name}", value=f"\n └Reason: {reason}")
+            await ctx.send(embed=embed)
+        except discord.errors.Forbidden:
+            await ctx.send("Looks like i dont have perms to use that command!")
+            return
+
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            NoPermArray = [
+                "You do nor have permission to use that command!",
+                "It appears you do not have permission to run that command!",
+                "I cannot ban that user due to your perms!",
+                "Sorry! Please check your perms and try again",
+            ]
+            await ctx.send(random.choice(NoPermArray))
+        return
 def setup(bot):
     bot.add_cog(Admin(bot))
