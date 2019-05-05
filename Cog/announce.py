3adbf35df9f25c891946b24a6c9fabd5daec0761
Sutinder S. Saini <38633228+Saini14@users.noreply.github.com>
2019-05-05 16:50:02 -0700

    Update announce.py

--- Cog/announce.py
+++ Cog/announce.py
@@ -1,11 +1,8 @@
 import discord
 from discord.ext import commands
-
-
 class announce(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def announce(self, ctx, *, announcement: str = None):
@@ -23,7 +20,6 @@ class announce(commands.Cog):
         await channel.send(embed=embed)
         await channel.send("@everyone")
         await ctx.channel.purge(limit=1)
-
 
 def setup(bot):
     bot.add_cog(announce(bot))
