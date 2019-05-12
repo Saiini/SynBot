c0005edc59da52d0f92552b8ec09d53dfd63c698
sutin <defsdfsdfsdfsf>
2019-05-11 21:05:36 -0700

    -

--- Cog/appeal.py
+++ Cog/appeal.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import sqlite3
 class appeal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -8,8 +7,10 @@ class appeal(commands.Cog):
         This really ugly code works for some strange reason...
         '''
     @commands.command()
+    #@commands.cooldown(rate=12, per=0)
     async def appeal(self, ctx):
         author = ctx.author
+        channel = self.bot.get_channel(576971106376744960)
         try:
             await ctx.message.add_reaction("📬")
             await ctx.send("*Sent you a dm!*")
@@ -26,12 +27,10 @@ class appeal(commands.Cog):
                             await ctx.author.send("Okay! Now that we have that out of the way, what type of ban did you recieve? [Perm/Temp]")
                             ban_type = await self.bot.wait_for('message', timeout=200)
                             try:
-                                channel = ctx.guild.get_channel(576971106376744960)
                                 await ctx.author.send("Oki-Doki! We have everything set! The staff members will look at your ban appeal shortly.\n Thank you for your time!")
                                 embed = discord.Embed(color=0x7C0A02)
                                 embed.add_field(name="Ban Appeal", value=f"IGN: *{ign.clean_content}*\n Reason for ban: *{reason.clean_content}*\n Ban type: *{ban_type.clean_content}*\n Discord: *{author}*")
                                 await channel.send(embed=embed)
-                                return
                             except Exception:
                                 return
                         if option.clean_content == 'stop':
