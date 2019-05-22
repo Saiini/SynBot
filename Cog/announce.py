a4309e04f6ba39478105de1bf9768291d5434c10
marshall <CortexPE@users.noreply.github.com>
2019-05-22 07:50:39 -0700

    Update announce.py

--- Cog/announce.py
+++ Cog/announce.py
@@ -9,17 +9,7 @@ class announce(commands.Cog):
     async def announce(self, ctx, *, announcement : str = None):
         """So you want to announce something huh? well... Do it!"""
         await ctx.send('Sending...')
-        header = ""
-        body = ""
-        footer = ""
         channel = ctx.guild.get_channel(492621819950661653)  # CHANGE IF THIS HAS BEEN REMOVED
-        announcer = ctx.message.author
-        embed = discord.Embed(color=0x010000)
-        header += f"{announcement}"
-        footer += f"\n - {announcer}"
-        embed.add_field(name=u"\u200B", value=header)
-        rpf = announcer.avatar_url
-        embed.set_footer(text=footer, icon_url=rpf)
-        await channel.send(embed=embed)
+        await channel.send(embed=discord.Embed(color=0x010000, title=u"\u200B", description=announcement))
 def setup(bot):
     bot.add_cog(announce(bot))
