39060e08256a15c1a3d31890ddaa0356972288ab
Saiini14 <38633228+Saiini@users.noreply.github.com>
2019-05-25 13:11:51 -0700

    Update MessageXP.py

--- Cog/XP/MessageXP.py
+++ Cog/XP/MessageXP.py
@@ -1,25 +1,28 @@
 import discord
 from discord.ext import commands
 import json
-import math
+import asyncio
+
+
 class MessageXP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        with open("STORE\XPSTORE.json", "r") as store:
+        with open(r"STORE\user.json", "r") as store:
             self.users = json.load(store)
+        self.bot.loop.create_task(self._save())
 
     async def _save(self):
         await self.bot.wait_until_ready()
-        while not self.bot.closed():
-            with open("STORE\XPSTORE.json", 'w') as store:
-                json.dump(self.users, indent=5)
+        while not self.bot.is_closed():
+            with open(r"STORE\user.json", 'w') as store:
+                json.dump(self.users, store, indent=5)
+            await asyncio.sleep(2)
 
-    def _levelup(self, author_id):
+    def _levelup(self, author_id, message):
         present_xp = self.users[author_id]['xp']
         present_lvl = self.users[author_id]['level']
-        if math.floor(present_xp / (3600 * max(present_lvl, 1))):
+        if present_xp >= round((4 * (present_lvl ** 3)) / 5):
             self.users[author_id]['level'] += 1
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -33,8 +36,6 @@ class MessageXP(commands.Cog):
             self.users[author_id]['level'] = 1
             self.users[author_id]['xp'] = 0
         self.users[author_id]['xp'] += 1
-        if self._levelup(author_id):
-            await message.channel.send(f"Hey {message.author.mention}! You have leveled up to: {self.users['level']}")
 
     @commands.command()
     async def level(self, ctx, member: discord.Member = None):
@@ -44,8 +45,8 @@ class MessageXP(commands.Cog):
             await ctx.send(f"{member.mention} does not have any lvls yet!")
         else:
             embed = discord.Embed(color=0x0000)
-            embed.add_field(name="Stats for this member", value=f"XP: {self.users[member_id]['xp']}\nLevel: {self.users[member_id]['level']}")
+            embed.add_field(name="Stats for this member", value=f"XP: {self.users[member_id]['xp']}\nLevel: {self.users[member_id]['level']}", inline=True)
             await ctx.send(embed=embed)
-            
+
 def setup(bot):
     bot.add_cog(MessageXP(bot))
