277d4c05f24c812ec52ba1820afcbf930793ff8c
Sutinder <38633228+Saiini@users.noreply.github.com>
2019-07-22 11:50:55 -0700

    Updated the announce command

--- Cog/announce.py
+++ Cog/announce.py
@@ -1,15 +1,17 @@
 import discord
 from discord.ext import commands
-
+from nltk import sent_tokenize
+#pip install nltk
 class announce(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def announce(self, ctx, *, announcement : str = None):
-        """So you want to announce something huh? well... Do it!"""
-        await ctx.send('Sending...')
-        channel = ctx.guild.get_channel(492621819950661653)  # CHANGE IF THIS HAS BEEN REMOVED
-        await channel.send(embed=discord.Embed(color=0x010000, title=u"\u200B", description=announcement))
+        vored_sentence = sent_tokenize(announcement)
+        channel = ctx.guild.get_channel(492621819950661653)
+        embed = discord.Embed(color=0xE3AA44, title=f"**{vored_sentence[0]}**", description=f"{' '.join(vored_sentence[1:])}")
+        await channel.send(embed=embed)
 def setup(bot):
     bot.add_cog(announce(bot))
