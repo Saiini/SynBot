8b6a0be735799ded208926431cfab51ec293d433
Saiini <38633228+Saiini@users.noreply.github.com>
2019-08-04 16:48:16 -0700

    uwu

--- Cog/scmd.py
+++ Cog/scmd.py
@@ -1,22 +1,24 @@
 # run server commands from the bot
 import discord
+import Cog.mcrcon as mcrcon
+import socket
 from discord.ext import commands
-from mcrcon import MCRcon
 class scmd(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
     @commands.command()
-    @commands.has_permissions(ban_members=True)
+    @commands.has_permissions(ban_members=True) # just in case something goes **__terribly__** wrong
     async def scmd(self, ctx, *, cmd : str = None):
-        if cmd == None:
-            # no command was provided
-            return
-        conn = MCRcon("", "")
-        conn.connect()
-        resp = conn.command(f"/{cmd}")
-        print(f"SENT: {cmd}")
-
+        if ctx.author.id == 570698736536125481 or ctx.author.id == 593294576026910720 or ctx.author.id == 181174394536722432: # check if we are either cort, erin, or saiini
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(("synhcf.net", 19132))
+                mcrcon.login(sock, 'ZxzDqzUUyB')  # Perform initial login
+                mcrcon.command(sock, cmd)
+            except:
+                return
+        else:
+            await ctx.send("Test failed!")
 def setup(bot):
     bot.add_cog(scmd(bot))
