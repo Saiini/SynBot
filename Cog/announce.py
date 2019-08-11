f2be17fe41cacbb7bfb4ca27ca453bf1901dd199
Saiini <38633228+Saiini@users.noreply.github.com>
2019-08-11 08:19:17 -0700

    ok loli boy i did it

--- Cog/announce.py
+++ Cog/announce.py
@@ -7,8 +7,9 @@ class announce(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def announce(self, ctx, *, announcement : str = None):
+        vored_sent = announcement.replace(".", "\n")
         channel = ctx.guild.get_channel(492621819950661653)
-        embed = discord.Embed(color=0xE3AA44, title=f"**{announcement.replace(".", "\n")}**")
+        embed = discord.Embed(color=0xE3AA44, title=vored_sent)
         await channel.send(embed=embed)
 def setup(bot):
     bot.add_cog(announce(bot))
