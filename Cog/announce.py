dac3a3344eba8a58962ff2943124b8e66227c8c1
Saiini <38633228+Saiini@users.noreply.github.com>
2019-08-11 08:10:58 -0700

    ok loli boy i did it

--- Cog/announce.py
+++ Cog/announce.py
@@ -1,7 +1,5 @@
 import discord
 from discord.ext import commands
-from nltk import sent_tokenize
-#pip install nltk
 class announce(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -9,9 +7,8 @@ class announce(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def announce(self, ctx, *, announcement : str = None):
-        vored_sentence = sent_tokenize(announcement)
         channel = ctx.guild.get_channel(492621819950661653)
-        embed = discord.Embed(color=0xE3AA44, title=f"**{vored_sentence[0]}**", description=f"{' '.join(vored_sentence[1:])}")
+        embed = discord.Embed(color=0xE3AA44, title=f"**{announcement.replace(".", "\n")}**")
         await channel.send(embed=embed)
 def setup(bot):
     bot.add_cog(announce(bot))
