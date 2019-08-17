3045d8c57dd6ca39701aac811f959e2b559ebbab
Saiini <38633228+Saiini@users.noreply.github.com>
2019-08-17 12:21:22 -0700

    edited in vim because im super cool

--- Cog/announce.py
+++ Cog/announce.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import re
+# i had to fix this shit in vim
+# - saiini
 class announce(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -7,9 +10,10 @@ class announce(commands.Cog):
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def announce(self, ctx, *, announcement : str = None):
-        vored_sent = announcement.replace(".", "\n")
+        enders = re.compile("[.!?]")
+        summonText = enders.split(announcement)
         channel = ctx.guild.get_channel(492621819950661653)
-        embed = discord.Embed(color=0xE3AA44, title=vored_sent)
+        embed = discord.Embed(color=0xE3AA44, title=f"**{summonText[0]}**", description=f"{''.join(summonText[1:])}")
         await channel.send(embed=embed)
 def setup(bot):
     bot.add_cog(announce(bot))
